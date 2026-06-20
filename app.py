85bf1ded904626fd88af99b1f9dd321fab68fb02
MOrtegaRivera <35515072+MOrtegaRivera@users.noreply.github.com>
2026-06-20 08:27:27 -0600

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("Hola mundo")
-st.write("Esta es mi primera aplicación desarrollada con Streamlit.")
-
-nombre = st.text_input("Escriba su nombre:")
-
-if nombre:
-    st.write(f"¡Hola, {nombre}!")
